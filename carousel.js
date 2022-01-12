b1b6299eb8af2b8253e226e1d45533cb6e594241
agarictree <ormhild@gmail.com>
2022-01-12 23:28:26 +0300

    fix container; refactor carousel code

--- carousel.js
+++ carousel.js
@@ -1,6 +1,61 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+let images = [
+    {
+        img: "img/hlZYIyrrx7E.jpg", 
+        srcSet: "img/hlZYIyrrx7E.jpg 1000w, img/hlZYIyrrx7E_500.jpg 500w, img/hlZYIyrrx7E_200.jpg 200w",
+        text: "Серьги 'Духи листьев'",
+        sizes: "(min-width: 1000px) 1000w, (max-width: 1000px) 500w, (max-width: 500px) 200w"
+    }, {
+        img: "img/I_PtqO_bbfc.jpg", 
+        srcSet: "img/I_PtqO_bbfc.jpg 1000w, img/I_PtqO_bbfc_500.jpg 500w, img/I_PtqO_bbfc_200.jpg 200w",
+        text: "Брошь 'Золото Инков'",
+        sizes: "(min-width: 1000px) 1000w, (max-width: 1000px) 500w, (max-width: 500px) 200w"
+    }, {
+        img: "img/jyoVDfkbbaE.jpg", 
+        srcSet: "img/jyoVDfkbbaE.jpg 1000w, img/jyoVDfkbbaE_500.jpg 500w, img/jyoVDfkbbaE_200.jpg 200w",
+        text: "Колье 'Медуза Горгона'",
+        sizes: "(min-width: 1000px) 1000w, (max-width: 1000px) 500w, (max-width: 500px) 200w"
+    }, {
+        img: "img/pxgBKof1kiE.jpg", 
+        srcSet: "img/pxgBKof1kiE.jpg 1080w, img/pxgBKof1kiE_500.jpg 500w, img/pxgBKof1kiE_200.jpg 200w",
+        text: "Броши 'Лесные фавны'",
+        sizes: "(min-width: 1000px) 1000w, (max-width: 1000px) 500w, (max-width: 500px) 200w"
+    }, {
+        img: "img/qRmItrc6KkM.jpg", 
+        srcSet: "img/qRmItrc6KkM.jpg 1000w, img/qRmItrc6KkM_500.jpg 500w, img/qRmItrc6KkM_200.jpg 200w",
+        text: "Колье 'Фея Чертополоха'",
+        sizes: "(min-width: 1000px) 1000w, (max-width: 1000px) 500w, (max-width: 500px) 200w"
+    }, {
+        img: "img/qz2HW3U8D2U.jpg", 
+        srcSet: "img/qz2HW3U8D2U.jpg 1000w, img/qz2HW3U8D2U_500.jpg 500w, img/qz2HW3U8D2U_200.jpg 200w",
+        text: "Брошь 'Лунная Фея'",
+        sizes: "(min-width: 1000px) 1000w, (max-width: 1000px) 500w, (max-width: 500px) 200w"
+    }, {
+        img: "img/VBWVXq641JI.jpg", 
+        srcSet: "img/VBWVXq641JI.jpg 640w, img/VBWVXq641JI.jpg_200.jpg 200w",
+        text: "Брошь 'Закат'",
+        sizes: "(min-width: 500px) 640w, (max-width: 500px) 200w"
+    }, {
+        img: "img/YAD2_sZtc7A.jpg", 
+        srcSet: "img/YAD2_sZtc7A.jpg 1000w, img/YAD2_sZtc7A_500.jpg 500w, img/YAD2_sZtc7A_200.jpg 200w",
+        text: "Колье 'Фея' из сплава 'пьютер'",
+        sizes: "(min-width: 1000px) 1000w, (max-width: 1000px) 500w, (max-width: 500px) 200w"
+    }
+]
+function createBlock(img, srcSet, text, sizes) {
+    return (
+        <div className="card">
+            <img src={img} alt="изделие" className="card-img-top p-2" srcSet={srcSet} sizes={sizes}/>
+                <div className="card-body d-flex flex-column justify-content-between p-2">
+                    <h5 className="card-title m-2">{text}</h5>
+                        <button className="align-self-end" id="toShop"><Link to="/shop">Посмотреть и купить</Link></button>
+                </div>
+        </div>
+    )
+}
+
 export default function Carousel() {
     function arrowRight(e) {
         let target = e.target;
@@ -31,62 +86,11 @@ export default function Carousel() {
                 <i className="bi bi-chevron-left"></i>
             </div>
         <section id="carousel">
-            <div className="card">
-                <img src="img/hlZYIyrrx7E.jpg" alt="изделие" className="card-img-top p-2" srcSet="img/hlZYIyrrx7E.jpg 1000w, img/hlZYIyrrx7E_500.jpg 500w, img/hlZYIyrrx7E_200.jpg 200w" sizes="(min-width: 1000px) 1000w, (max-width: 1000px) 500w, (max-width: 500px) 200w"/>
-                <div className="card-body d-flex flex-column justify-content-between p-2">
-                <h5 className="card-title m-2">Серьги "Духи листьев"</h5>
-                    <button className="align-self-end" id="toShop"><Link to="/shop">Посмотреть и купить</Link></button>
-                    </div>
-            </div>
-            <div className="card">
-                <img src="img/I_PtqO_bbfc.jpg" className="card-img-top p-2" alt="изделие"  srcSet="img/I_PtqO_bbfc.jpg 1000w, img/I_PtqO_bbfc_500.jpg 500w, img/I_PtqO_bbfc_200.jpg 200w" sizes="(min-width: 1000px) 1000w, (max-width: 1000px) 500w, (max-width: 500px) 200w"/>
-                <div className="card-body d-flex flex-column justify-content-between p-2">
-                    <h5 className="card-title m-2">Брошь "Золото Инков"</h5>
-                    <button className="align-self-end" id="toShop"><Link to="/shop">Посмотреть и купить</Link></button>
-                </div>
-            </div>
-            <div className="card">
-                <img src="img/jyoVDfkbbaE.jpg" className="card-img-top p-2" alt="изделие"  srcSet="img/jyoVDfkbbaE.jpg 1000w, img/jyoVDfkbbaE_500.jpg 500w, img/jyoVDfkbbaE_200.jpg 200w" sizes="(min-width: 1000px) 1000w, (max-width: 1000px) 500w, (max-width: 500px) 200w"/>
-                <div className="card-body d-flex flex-column justify-content-between p-2">
-                    <h5 className="card-title m-2">Колье "Медуза Горгона"</h5>
-                    <button className="align-self-end" id="toShop"><Link to="/shop">Посмотреть и купить</Link></button>
-                </div>
-            </div>
-            <div className="card">
-                <img src="img/pxgBKof1kiE.jpg" className="card-img-top p-2" alt="изделие"  srcSet="img/pxgBKof1kiE.jpg 1080w, img/pxgBKof1kiE_500.jpg 500w, img/pxgBKof1kiE_200.jpg 200w" sizes="(min-width: 1000px) 1000w, (max-width: 1000px) 500w, (max-width: 500px) 200w"/>
-                <div className="card-body d-flex flex-column justify-content-between p-2">
-                    <h5 className="card-title m-2">Броши "Лестные фавны"</h5>
-                    <button className="align-self-end" id="toShop"><Link to="/shop">Посмотреть и купить</Link></button>
-                </div>
-            </div>
-            <div className="card">
-                <img src="img/qRmItrc6KkM.jpg" className="card-img-top p-2" alt="изделие"  srcSet="img/qRmItrc6KkM.jpg 1000w, img/qRmItrc6KkM_500.jpg 500w, img/qRmItrc6KkM_200.jpg 200w" sizes="(min-width: 1000px) 1000w, (max-width: 1000px) 500w, (max-width: 500px) 200w"/>
-                <div className="card-body d-flex flex-column justify-content-between p-2">
-                    <h5 className="card-title m-2">Колье "Фея Чертополоха"</h5>
-                    <button className="align-self-end" id="toShop"><Link to="/shop">Посмотреть и купить</Link></button>
-                </div>
-            </div>
-            <div className="card">
-                <img src="img/qz2HW3U8D2U.jpg" className="card-img-top p-2" alt="изделие"  srcSet="img/qz2HW3U8D2U.jpg 1000w, img/qz2HW3U8D2U_500.jpg 500w, img/qz2HW3U8D2U_200.jpg 200w" sizes="(min-width: 1000px) 1000w, (max-width: 1000px) 500w, (max-width: 500px) 200w"/>
-                <div className="card-body d-flex flex-column justify-content-between p-2">
-                    <h5 className="card-title m-2">Брошь "Лунная Фея"</h5>
-                    <button className="align-self-end" id="toShop"><Link to="/shop">Посмотреть и купить</Link></button>
-                </div>
-            </div>
-            <div className="card">
-                <img src="img/VBWVXq641JI.jpg" className="card-img-top p-2" alt="изделие"  srcSet="img/VBWVXq641JI.jpg 640w, img/VBWVXq641JI.jpg_200.jpg 200w" sizes="(min-width: 500px) 640w, (max-width: 500px) 200w"/>
-                <div className="card-body d-flex flex-column justify-content-between p-2">
-                    <h5 className="card-title m-2">Брошь "Закат"</h5>
-                    <button className="align-self-end" id="toShop"><Link to="/shop">Посмотреть и купить</Link></button>
-                </div>
-            </div>
-            <div className="card">
-                <img src="img/YAD2_sZtc7A.jpg" className="card-img-top p-2" alt="изделие"  srcSet="img/YAD2_sZtc7A.jpg 1000w, img/YAD2_sZtc7A_500.jpg 500w, img/YAD2_sZtc7A_200.jpg 200w" sizes="(min-width: 1000px) 1000w, (max-width: 1000px) 500w, (max-width: 500px) 200w"/>
-                <div className="card-body d-flex flex-column justify-content-between p-2">
-                    <h5 className="card-title m-2">Колье "Фея" из сплава "пьютер"</h5>
-                    <button className="align-self-end" id="toShop"><Link to="/shop">Посмотреть и купить</Link></button>
-                </div>
-            </div>
+            {
+                images.map(item => {
+                    return createBlock(item.img, item.srcSet, item.text, item.sizes);
+                })
+            }
         </section>
         <div className="arrow arrowRight" onClick={arrowRight}>
                 <i className="bi bi-chevron-right"></i>
--- footer.js
+++ footer.js
@@ -1,42 +1,19 @@
 import React from "react";
 
 export default function Footer() {
-    function onMouseEnter(e) {
-        let text;
-        if(e.target.tagName == "A") {
-            text = e.target.id;
-        }
-        let tooltip = document.getElementsByClassName("tooltip-general")[0];
-        tooltip.hidden = false;
-        tooltip.textContent = text;
-        let coords = e.target.getBoundingClientRect();
-        tooltip.style.top = 0 - 25 + "px";
-        if(tooltip.offsetWidth < coords.width) {
-            tooltip.style.left = coords.left + (tooltip.offsetWidth / 2) + "px";
-        } else {
-            tooltip.style.left = coords.left - (coords.width / 4) + "px";
-            
-        }
-    }
-    function onMouseLeave(e) {
-        let tooltip = document.getElementsByClassName("tooltip-general")[0];
-        if(e.target.tagName == "A") {
-            tooltip.hidden = true;
-        }
-    }
     return (
         <footer>
             <p>Мы в социальных сетях</p>
             <div id="container">
             <div id="icons">
                 <div className="tooltip-general" hidden></div>
-                <a href="https://www.instagram.com/yulekottur/" target="_blank" id="instagram"  onMouseEnter={onMouseEnter} onMouseLeave={onMouseLeave}>
+                <a href="https://www.instagram.com/yulekottur/" target="_blank" id="instagram" title="Instagram">
                     <img src="./img/ig_40.png" alt="instagram logo" />
                 </a>
-                <a href="https://vk.com/yulekottur" target="_blank" id="vk" onMouseEnter={onMouseEnter} onMouseLeave={onMouseLeave}>
+                <a href="https://vk.com/yulekottur" target="_blank" id="vk" title="VK">
                     <img src="./img/vk_40.png" alt="vk logo" />
                 </a>
-                <a href="https://www.livemaster.ru/yulekottur/" target="_blank" id="livemaster" onMouseEnter={onMouseEnter} onMouseLeave={onMouseLeave}>
+                <a href="https://www.livemaster.ru/yulekottur/" target="_blank" id="livemaster" title="Livemaster">
                     <img src="./img/lm_40.png" alt="livemaster logo" />
                 </a>
             </div>
